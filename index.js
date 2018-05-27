7063ec351311062fe6a6683dbc1937605d743d1d
Palash gupta <palashg7563@gmail.com>
2018-05-28 03:27:15 +0530

    completed circle with animation

--- index.js
+++ index.js
@@ -5,6 +5,53 @@ canva.height = window.innerHeight;
 
 const contest = canva.getContext("2d");
 
-contest.fillRect(10, 10, 40, 40);
+function Circle(x, y, dx, dy, radius) {
+  this.x = x;
+  this.y = y;
+  this.dx = dx;
+  this.dy = dy;
+  this.radius = radius;
 
-console.log(canva);
+  this.draw = function() {
+    contest.beginPath();
+    contest.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    contest.strokeStyle = "red";
+    contest.stroke();
+  };
+
+  this.update = function() {
+    if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
+      this.dx = -this.dx;
+    }
+
+    if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
+      this.dy = -this.dy;
+    }
+    this.x += this.dx;
+    this.y += this.dy;
+
+    this.draw();
+  };
+}
+
+const circleArray = [];
+for (let i = 0; i < 100; i++) {
+  let x = Math.random() * innerWidth;
+  let y = Math.random() * innerHeight;
+  let dx = (Math.random() - 0.5) * 10;
+  let dy = (Math.random() - 0.5) * 10;
+  let radius = 30;
+
+  circleArray.push(new Circle(x, y, dx, dy, radius));
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+  contest.clearRect(0, 0, innerWidth, innerHeight);
+
+  for (let i = 0; i < circleArray.length; i++) {
+    circleArray[i].update();
+  }
+}
+
+animate();
